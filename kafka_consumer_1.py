82d957273fd083c7f64a0e5ae024dda816e937e6
Sanjay Kumar Sahu <79205856+Sanjay-Sahu811@users.noreply.github.com>
2022-10-30 23:51:36 +0530

    Add files via upload

---
+++ kafka_consumer_1.py
@@ -0,0 +1,93 @@
+import argparse
+
+from confluent_kafka import Consumer
+from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.schema_registry.json_schema import JSONDeserializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+
+API_KEY = 'T7XPBIUBFC5EJQ3Y'
+ENDPOINT_SCHEMA_URL = 'https://psrc-8kz20.us-east-2.aws.confluent.cloud'
+API_SECRET_KEY = 'FygDbH5cjoZLtjWayDuMy+YJmQKVMGNZCmUqLBPMz6icNECGumfEAr4vhSvJoOuU'
+BOOTSTRAP_SERVER = 'pkc-ymrq7.us-east-2.aws.confluent.cloud:9092'
+SECURITY_PROTOCOL = 'SASL_SSL'
+SSL_MACHENISM = 'PLAIN'
+SCHEMA_REGISTRY_API_KEY = 'DMY2YYOBDVT5FET3'
+SCHEMA_REGISTRY_API_SECRET = 'CFTxt2MgkxgswANbgOODuoPgXTbeEplCo49N9xdEBHLw9RR4aiH187BvgONRgAT3'
+
+
+def sasl_conf():
+    sasl_conf = {'sasl.mechanism': SSL_MACHENISM,
+                 # Set to SASL_SSL to enable TLS support.
+                 #  'security.protocol': 'SASL_PLAINTEXT'}
+                 'bootstrap.servers': BOOTSTRAP_SERVER,
+                 'security.protocol': SECURITY_PROTOCOL,
+                 'sasl.username': API_KEY,
+                 'sasl.password': API_SECRET_KEY
+                 }
+    return sasl_conf
+
+
+def schema_config():
+    return {'url': ENDPOINT_SCHEMA_URL,
+
+            'basic.auth.user.info': f"{SCHEMA_REGISTRY_API_KEY}:{SCHEMA_REGISTRY_API_SECRET}"
+
+            }
+
+
+class Order:
+    def __init__(self, record: dict):
+        for k, v in record.items():
+            setattr(self, k, v)
+
+        self.record = record
+
+    @staticmethod
+    def dict_to_order(data: dict, ctx):
+        return Order(record=data)
+
+    def __str__(self):
+        return f"{self.record}"
+
+schema_registry_conf = schema_config()
+schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+def main(topic):
+    subjects = schema_registry_client.get_subjects()
+    for subject in subjects:
+        if subject == 'restaurant-take-away-data-value':
+            schema = schema_registry_client.get_latest_version(subject)
+            schema_str = schema.schema.schema_str
+
+
+    json_deserializer = JSONDeserializer(schema_str, from_dict=Order.dict_to_order)
+
+    consumer_conf = sasl_conf()
+    consumer_conf.update({
+        'group.id': 'group1',
+        'auto.offset.reset': "earliest"})
+
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe([topic])
+    group1Count = 0
+    while True:
+        try:
+            # SIGINT can't be handled when polling, limit timeout to 1 second.
+            msg = consumer.poll(1.0)
+            if msg is None:
+                continue
+
+            order = json_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
+
+            if order is not None:
+                print("Count {}: User record {}: order: {}\n"
+                      .format(group1Count, msg.key(), order))
+            group1Count+=1
+
+        except KeyboardInterrupt:
+            break
+
+    consumer.close()
+
+
+main("restaurant-take-away-data")
---
+++ restaurant_producer.py
@@ -0,0 +1,144 @@
+from uuid import uuid4
+from confluent_kafka import Producer
+from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.json_schema import JSONSerializer
+import pandas as pd
+from typing import List
+
+FILE_PATH = '/inueron/Apache Kafka/restaurant_orders.csv'
+
+columns= ['Order Number','Order Date','Item Name','Quantity','Product Price','Total products']
+
+API_KEY = 'T7XPBIUBFC5EJQ3Y'
+ENDPOINT_SCHEMA_URL = 'https://psrc-8kz20.us-east-2.aws.confluent.cloud'
+API_SECRET_KEY = 'FygDbH5cjoZLtjWayDuMy+YJmQKVMGNZCmUqLBPMz6icNECGumfEAr4vhSvJoOuU'
+BOOTSTRAP_SERVER = 'pkc-ymrq7.us-east-2.aws.confluent.cloud:9092'
+SECURITY_PROTOCOL = 'SASL_SSL'
+SSL_MACHENISM = 'PLAIN'
+SCHEMA_REGISTRY_API_KEY = 'DMY2YYOBDVT5FET3'
+SCHEMA_REGISTRY_API_SECRET = 'CFTxt2MgkxgswANbgOODuoPgXTbeEplCo49N9xdEBHLw9RR4aiH187BvgONRgAT3'
+
+
+def sasl_conf():
+    sasl_conf = {'sasl.mechanism': SSL_MACHENISM,
+                 # Set to SASL_SSL to enable TLS support.
+                 #  'security.protocol': 'SASL_PLAINTEXT'}
+                 'bootstrap.servers': BOOTSTRAP_SERVER,
+                 'security.protocol': SECURITY_PROTOCOL,
+                 'sasl.username': API_KEY,
+                 'sasl.password': API_SECRET_KEY
+                 }
+    return sasl_conf
+
+
+def schema_config():
+    return {'url': ENDPOINT_SCHEMA_URL,
+
+            'basic.auth.user.info': f"{SCHEMA_REGISTRY_API_KEY}:{SCHEMA_REGISTRY_API_SECRET}"
+
+            }
+
+class Order:
+    def __init__(self, record: dict):
+        for k, v in record.items():
+            setattr(self, k, v)
+
+        self.record = record
+
+    @staticmethod
+    def dict_to_order(data: dict, ctx):
+        return Order(record=data)
+
+    def __str__(self):
+        return f"{self.record}"
+
+
+def get_order_instance(file_path):
+    df = pd.read_csv(FILE_PATH)
+    df = df.iloc[:]
+    Orders = []
+    for data in df.values:
+        order = Order(dict(zip(columns, data)))
+        Orders.append(order)
+        yield order
+
+
+def order_to_dict(order:Order, ctx):
+    """
+    Returns a dict representation of a User instance for serialization.
+    Args:
+        user (User): User instance.
+        ctx (SerializationContext): Metadata pertaining to the serialization
+            operation.
+    Returns:
+        dict: Dict populated with user attributes to be serialized.
+    """
+
+    # User._address must not be serialized; omit from dict
+    return order.record
+
+
+def delivery_report(err, msg):
+    """
+    Reports the success or failure of a message delivery.
+    Args:
+        err (KafkaError): The error that occurred on None on success.
+        msg (Message): The message that was produced or failed.
+    """
+
+    if err is not None:
+        print("Delivery failed for User record {}: {}".format(msg.key(), err))
+        return
+    print('User record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+
+schema_registry_conf = schema_config()
+schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+def main(topic):
+    subjects = schema_registry_client.get_subjects()
+    for subject in subjects:
+        if subject == 'restaurant-take-away-data-value':
+            schema = schema_registry_client.get_latest_version(subject)
+            schema_str = schema.schema.schema_str
+
+
+
+
+
+
+    string_serializer = StringSerializer('utf_8')
+    json_serializer = JSONSerializer(schema_str, schema_registry_client, order_to_dict)
+
+    producer = Producer(sasl_conf())
+
+    print("Producing user records to topic {}. ^C to exit.".format(topic))
+    # while True:
+    # Serve on_delivery callbacks from previous calls to produce()
+    producer.poll(0.0)
+    try:
+        for order in get_order_instance(file_path=FILE_PATH):
+            print(order)
+            producer.produce(topic=topic,
+                             key=string_serializer(str(uuid4()), order_to_dict),
+                             value=json_serializer(order, SerializationContext(topic, MessageField.VALUE)),
+                             on_delivery=delivery_report)
+
+
+
+    except KeyboardInterrupt:
+        pass
+    except ValueError:
+        print("Invalid input, discarding record...")
+        pass
+
+    print("\nFlushing records...")
+    producer.flush()
+
+main('restaurant-take-away-data')
+
+
+
+
+
